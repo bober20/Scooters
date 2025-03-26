479192de8dce5d343b83d1d069ef03ae0d887e55
Darya Saroka <dara@MacBook-Air-Darya.local>
2025-03-26 10:57:23 +0300

    Repositories fixes. Ride's repo functions added

--- Scooters/Application/Interfaces/Repositories/IRideRepository.cs
+++ Scooters/Application/Interfaces/Repositories/IRideRepository.cs
@@ -3,4 +3,6 @@ namespace Application.Interfaces.Repositories;
 public interface IRideRepository
 {
     Task<Ride?> GetRidesByUserIdAsync();
+    Task CreateRideAsync(Ride ride);
+    Task DeleteRideAsync(Guid rideId);
 }
--- Scooters/Application/Interfaces/Repositories/IScooterRepository.cs
+++ Scooters/Application/Interfaces/Repositories/IScooterRepository.cs
@@ -5,5 +5,5 @@ public interface IScooterRepository
     Task<Scooter?> GetScooterByIdAsync(Guid id);
     Task CreateScooterAsync(Scooter scooter);
     Task DeleteScooterAsync(Guid id);
-    Task UpdateScooterAsync(Guid id);
+    Task UpdateScooterAsync(Scooter scooter);
 }
