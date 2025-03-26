f3aa36b9dc57c9615f9f84a9455b80a17aebce8e
Darya Saroka <dara@MacBook-Air-Darya.local>
2025-03-26 11:11:54 +0300

    Repositories structure changed

--- Scooters/Application/Interfaces/Repositories/IReservationRepository.cs
+++ Scooters/Application/Interfaces/Repositories/IReservationRepository.cs
@@ -2,9 +2,9 @@ namespace Application.Interfaces.Repositories;
 
 public interface IReservationRepository
 {
-    Task<Ride?> GetRidesByByIdAsync(Guid id);
-    Task<Reservation?> GetReservationByUserIdAsync(Guid userId);
-    Task<Reservation?> GetReservationByScooterIdAsync(Guid scooterId);
+    Task<Reservation?> GetReservationByIdAsync(Guid id);
+    Task<List<Reservation>?> GetReservationsByUserIdAsync(Guid userId);
+    Task<List<Reservation>?> GetReservationsByScooterIdAsync(Guid scooterId);
     Task CreateReservationAsync(Reservation reservation);
     Task DeleteReservationAsync(Guid reservationId);
 }
--- Scooters/Application/Interfaces/Repositories/IRideRepository.cs
+++ Scooters/Application/Interfaces/Repositories/IRideRepository.cs
@@ -2,7 +2,10 @@ namespace Application.Interfaces.Repositories;
 
 public interface IRideRepository
 {
-    Task<Ride?> GetRidesByUserIdAsync(Guid userId);
+    Task<Ride?> GetRideById(Guid id);
+    Task<List<Ride>?> GetRidesByScooterIdAsync(Guid scooterId);
+    Task<List<Ride>?> GetRidesByUserIdAsync(Guid userId);
     Task CreateRideAsync(Ride ride);
     Task DeleteRideAsync(Guid rideId);
+    Task UpdateRideAsync(Ride ride);
 }
