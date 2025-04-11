1ae2254308f49b919434d3193078ac47cf80aa6f
Darya Saroka <dara@MacBook-Air-Darya.local>
2025-04-11 17:22:16 +0300

    QR page finished

--- Scooters/Scooters/ViewModels/QRScannerViewModel.cs
+++ Scooters/Scooters/ViewModels/QRScannerViewModel.cs
@@ -1,13 +1,45 @@
+using System.Net;
+using Application.Common.Interfaces.NavigationService;
+using CommunityToolkit.Maui.Core;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using ZXing;
+using ZXing.Net.Maui;
 
 namespace Scooters.ViewModels;
 
 public partial class QRScannerViewModel : ObservableObject
 {
-    [RelayCommand]
-    private async Task BarcodeDetected()
+    [ObservableProperty] private Result _result;
+    [ObservableProperty] private bool _isDetecting = true;
+
+    private readonly INavigationService _navigationService;
+    private readonly IPopupService _popupService;
+
+    public QRScannerViewModel(INavigationService navigationService, IPopupService popupService)
     {
+        _navigationService = navigationService;
+        _popupService = popupService;
+    }
+
+    [RelayCommand]
+    private async Task BarcodeDetected(BarcodeDetectionEventArgs e)
+    {
+        IsDetecting = false;
+        var barcode = e.Results.FirstOrDefault();
+        if (barcode == null)
+        {
+            IsDetecting = true;
+            return;
+        }
+        Guid.TryParse(barcode.Value, out var barcodeGuid);
         
+        MainThread.BeginInvokeOnMainThread(() =>
+        {
+            _popupService.ShowPopupAsync<ReservationViewModel>(
+                onPresenting: viewModel => viewModel.ScooterId = barcodeGuid);
+        });
+        
+        IsDetecting = true;
     }
 }
--- Scooters/Scooters/Views/QRScannerPage.xaml.cs
+++ Scooters/Scooters/Views/QRScannerPage.xaml.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Scooters.ViewModels;
+using ZXing.Net.Maui;
 
 namespace Scooters.Views;
 
 public partial class QRScannerPage : ContentPage
 {
-    public QRScannerPage()
+    public QRScannerPage(QRScannerViewModel viewModel)
     {
         InitializeComponent();
+        BindingContext = viewModel;
     }
 }
