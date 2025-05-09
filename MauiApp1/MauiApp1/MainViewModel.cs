bc318894c6906912e38e391b7a52531ab3532234
Darya <dsoroka782@gmail.com>
2025-05-09 15:46:32 +0300

    Source binding

--- MauiApp1/MauiApp1/MainViewModel.cs
+++ MauiApp1/MauiApp1/MainViewModel.cs
@@ -6,5 +6,5 @@ namespace MauiApp1;
 public partial class MainViewModel : ObservableObject
 {
     [ObservableProperty] private List<int> _items = [1, 2, 3,45,6,7,4,353,453];
-    [ObservableProperty] private string _placeholder = "fdjsfhkdf";
+    [ObservableProperty] private string _ppp = "fdjsfhkdf";
 }
